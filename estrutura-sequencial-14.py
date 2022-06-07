6601f1036525466eb2198a037df9c743e72582f0
Kauan Alves <107011580+kauannzz@users.noreply.github.com>
2022-06-07 11:58:19 -0300

    Update estrutura-sequencial-14.py

--- estrutura-sequencial-14.py
+++ estrutura-sequencial-14.py
@@ -2,6 +2,7 @@
 peso_peixes = float(input('Digite o peso do peixe: '))
 
 if(peso_peixes > 50):
+    #Lógica pra calcular o valor do excesso do peso do peixe e o valor da multa
     excesso = peso_peixes - 50
     multa = excesso * 4
 
