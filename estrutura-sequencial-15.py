c76b6a1c2d703eba608e53036b252621847fa6b0
Kauan Alves <107011580+kauannzz@users.noreply.github.com>
2022-06-07 10:35:36 -0300

    Adicionado no dia 07/06 as 10:35

---
+++ estrutura-sequencial-15.py
@@ -0,0 +1,22 @@
+from time import sleep
+
+valor_hora = float(input('Quanto você ganha por hora: '))
+numero_mes = float(input('Quantas horas você trabalha por mês: '))
+
+salario = valor_hora * numero_mes
+
+print('Salário bruto:', salario)
+print('Calculando o que você terá que pagar ao governo... ')
+sleep(2)
+
+imposto_renda = salario * 0.11 #Porcentagem de 11%
+print('Imposto de Renda (11%): R$', imposto_renda.__round__(2))
+
+inss = salario * 0.08 #Porcentagem de 8%
+print('INSS: (8%) R$:', inss.__round__(2))
+
+sindicato = salario * 0.05 #Porcentagem de 5%
+print('SINDICATO (5%) R$', sindicato.__round__(2))
+
+salario_liquido = salario - imposto_renda - inss - sindicato #Salário liquido
+print('Salário liquido: R$', salario_liquido.__round__(2))
